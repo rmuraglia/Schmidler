8be67c17cae08558a5fcd0e98a33cce7d6c9c4a5
Ryan Muraglia <rmuraglia@gmail.com>
2016-06-10 18:01:03 -0400

    convergence monitoring visualization
    added plotting functionality to look at progression of solution paths
    over the course of QL for this maze grid. 10 repetitions were saved to
    local images.

--- Q-learning/norm-norm-2d/ql_master.r
+++ Q-learning/norm-norm-2d/ql_master.r
@@ -61,9 +61,51 @@ source('ql_algorithm.r')
 tim<-ql_full_history(q_map, r_map, alpha, gamma, epsilon_init, epsilon_tau)
 
 
-# soln<-ql_path_soln(tim[[1]])
-# soln
-# ql_path_cost(tim[[2]], soln)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(gplots)
+
+num_solns<-length(unique(tim[[1]]))
+
+plot_df<-array(NA, dim=c(200, 3))
+colnames(plot_df)<-c('iter', 'cost', 'soln')
+plot_df[,1]<-c(1:200)
+plot_df[,2]<-tim[[2]]
+for (i in 1:num_solns) {
+    inds<-which(sapply(tim[[1]], identical, unique(tim[[1]])[[i]]))
+    plot_df[inds,3]<-i
+}
+plot_df<-as.data.frame(plot_df)
+plot_df[,3]<-as.factor(plot_df[,3])
+
+pcolors<-c('black', 'black', rich.colors(num_solns-2))
+pcolors<-c('black',rich.colors(num_solns-1))
+
+p0<-ggplot(data=plot_df, aes(x=iter, y=cost, colour=soln)) + geom_point() + theme(legend.position='top') + scale_colour_manual(values=pcolors)
+
+p2<-ggplot(data=point_grid, aes(x=lambda, y=temperature)) + geom_path(data=as.data.frame(unique(tim[[1]])[[2]]), arrow=arrow(), colour=pcolors[2], size=3) + geom_point() + labs(title='soln 2')
+
+p3<-ggplot(data=point_grid, aes(x=lambda, y=temperature)) + geom_path(data=as.data.frame(unique(tim[[1]])[[3]]), arrow=arrow(), colour=pcolors[3], size=3) + geom_point() + labs(title='soln 3')
+
+p4<-ggplot(data=point_grid, aes(x=lambda, y=temperature)) + geom_path(data=as.data.frame(unique(tim[[1]])[[4]]), arrow=arrow(), colour=pcolors[4], size=3) + geom_point() + labs(title='soln 4')
+
+p5<-ggplot(data=point_grid, aes(x=lambda, y=temperature)) + geom_path(data=as.data.frame(unique(tim[[1]])[[5]]), arrow=arrow(), colour=pcolors[5], size=3) + geom_point() + labs(title='soln 5')
+
+p6<-ggplot(data=point_grid, aes(x=lambda, y=temperature)) + geom_path(data=as.data.frame(unique(tim[[1]])[[6]]), arrow=arrow(), colour=pcolors[6], size=3) + geom_point() + labs(title='soln 6')
+
+p7<-ggplot(data=point_grid, aes(x=lambda, y=temperature)) + geom_path(data=as.data.frame(unique(tim[[1]])[[7]]), arrow=arrow(), colour=pcolors[7], size=3) + geom_point() + labs(title='soln 7')
+
+p8<-ggplot(data=point_grid, aes(x=lambda, y=temperature)) + geom_path(data=as.data.frame(unique(tim[[1]])[[8]]), arrow=arrow(), colour=pcolors[8], size=3) + geom_point() + labs(title='soln 8')
+
+p9<-ggplot(data=point_grid, aes(x=lambda, y=temperature)) + geom_path(data=as.data.frame(unique(tim[[1]])[[9]]), arrow=arrow(), colour=pcolors[9], size=3) + geom_point() + labs(title='soln 9')
+
+p10<-ggplot(data=point_grid, aes(x=lambda, y=temperature)) + geom_path(data=as.data.frame(unique(tim[[1]])[[10]]), arrow=arrow(), colour=pcolors[10], size=3) + geom_point() + labs(title='soln 10')
 
 
-# ql_path_cost2(tim[[2]], soln)
+outplot<-arrangeGrob(p0, p3, p4, p5, p6, p7, nrow=2, widths=c(1,1,1))
+
+ggsave(file='maze-grid01-monitor-10.png', width=9, height=6, dpi=100, plot=outplot)
+ggsave(file='maze-grid01-monitor-05.png', width=9, height=9, dpi=100, plot=outplot)
+ggsave(file='maze-grid01-monitor-07.png', width=7, height=6, dpi=100, plot=outplot)
+
