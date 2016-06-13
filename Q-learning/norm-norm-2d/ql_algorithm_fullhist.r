4f949732c37e57d7afeb32a25adad4b8ed890a00
Ryan Muraglia <rmuraglia@gmail.com>
2016-06-12 22:59:01 -0400

    create alternate files for full history and maze
    duplicate some files from older history to enable easier switching back
    and forth between full history monitoring and 3chain convergence

---
+++ Q-learning/norm-norm-2d/ql_algorithm_fullhist.r
@@ -0,0 +1,223 @@
+# ql_algorithm.r
+
+# to be called by ql_master.r
+# define Q-learning algorithm. adapted to minimize cost instead of maximize reward
+
+# add a piece to ql_search that checks for convergence
+# crtieria: steady opt path
+# criteria: steady path cost
+# note: need function to return opt path (handle incomplete ones)
+# note: need to store prev solns for comparison
+# note: need new handling for epsilon - no longer based on a static schedule since we don't know total num of waves
+
+ql_search<-function(q_map, r_map, alpha, gamma, epsilon_init, min_episode, conv_tol) {
+
+    # run QL episodes until path and path cost converge
+    maps<-list(q_map, r_map)
+    
+    # run the minimum number of searches
+    for (ql_iter in 1:min_episode) {
+        print(ql_iter)
+        maps<-ql_episode(maps[[1]], maps[[2]], (i-1)*epsilon_delta)
+    }
+
+    # calculate the current optimal path and cost
+    prev_path<-ql_path_soln(maps[[1]])
+    prev_cost<-ql_path_cost(maps[[2]], prev_path)    
+
+    # set convergence flags to false
+    path_conv<-FALSE
+    cost_conv<-FALSE
+    epsilon_curr<-0
+
+    while (!(path_conv & cost_conv)) { # until both are converged, keep going
+        ql_iter<-ql_iter+1
+        print(ql_iter)
+
+        # run an additional round and update info
+        maps<-ql_episode(maps[[1]], maps[[2]], epsilon_curr)
+        curr_path<-ql_path_soln(maps[[1]])
+        curr_cost<-ql_path_cost(maps[[2]], curr_path)
+
+        # check for convergence
+        path_conv<-identical(prev_path, curr_path)
+        cost_delta<-abs((curr_cost-prev_cost)/prev_cost)
+        cost_conv<-(cost_delta<conv_tol)
+
+        # update info for next round for searching
+        prev_path<-curr_path
+        prev_cost<-curr_cost
+        epsilon_curr<-epsilon_update(epsilon_curr, path_conv, cost_delta)
+    }
+    return(maps)
+}
+
+ql_full_history<-function(q_map, r_map, alpha, gamma, epsilon_init, epsilon_tau) {
+
+    maps<-list(q_map, r_map)
+    delta_epsilon<-1/epsilon_tau
+    path_solns<-vector('list', length=num_episode)
+    path_costs<-rep(NA, length.out=num_episode)
+
+    for (i in 1:epsilon_tau) {
+        print(i)
+        maps<-ql_episode(maps[[1]], maps[[2]], epsilon_init+(i-1)*delta_epsilon)
+        path_soln_temp<-ql_path_soln(maps[[1]])
+        path_cost_temp<-ql_path_cost(maps[[2]], path_soln_temp)
+        path_soln_mat<-t(sapply(path_soln_temp, split_func))
+        if (!is.na(path_soln_mat)) { colnames(path_soln_mat)<-c('lambda', 'temperature') }
+        path_solns[[i]]<-path_soln_mat
+        path_costs[i]<-path_cost_temp
+    }
+    for (i in (epsilon_tau+1):num_episode) {
+        print(i)
+        maps<-ql_episode(maps[[1]], maps[[2]], 1)
+        path_soln_temp<-ql_path_soln(maps[[1]])
+        path_cost_temp<-ql_path_cost(maps[[2]], path_soln_temp)
+        path_soln_mat<-t(sapply(path_soln_temp, split_func))
+        colnames(path_soln_mat)<-c('lambda', 'temperature')
+        path_solns[[i]]<-path_soln_mat
+        path_costs[i]<-path_cost_temp
+    }
+    return(list(path_solns, path_costs))
+}
+
+## ql_episode subroutine represents a single episode of learning
+# each wave explores a single path using one bundle of samples
+# inputs: 
+# r_map : list by indexer (one entry per node), within list, place list by adjacent nodes, place n x 2 matrix for varBAR and varvarBAR for each bundle's evaluation
+# q_map : list by indexer (one entry per node), each list entry  is vector of q values to neighbors
+
+ql_episode<-function(q_map, r_map, epsilon) {
+
+    # initialize this episode's search agent
+    # agent holds info on current path and draws
+    # at end, reprocess agent draws to determine var of each edge
+    # report vars as rewards and update q map
+    agent_path<-vector()
+
+    agent_path[1]<-indexer_init
+    iter_dummy<-0
+
+    while (!identical(agent_path[length(agent_path)], indexer_target)) { # until the agent has reached the target, continue the search
+
+        iter_dummy<-iter_dummy+1
+
+        # set current state
+        state_current<-agent_path[length(agent_path)]
+        curr_index<-which(indexer==state_current)
+
+        # get valid moves and q scores for those moves
+        full_q<-q_map[[curr_index]]
+        trim_index<-which(full_q[,1] %in% agent_path)
+        if (length(trim_index)!=0) { trim_q<-full_q[-trim_index, , drop=FALSE] 
+        } else { trim_q<-full_q }
+
+        # select between those moves based on q score policy
+        indexer_next<-q_choice(trim_q, epsilon)
+        next_index<-which(names(r_map[[curr_index]])==indexer_next)
+
+        # add choice to path
+        agent_path[iter_dummy+1]<-indexer_next
+
+        # draw new edge reward from distribution
+        reward_entry<-c(rnorm(1, r_distns[[curr_index]][next_index,1], r_distns[[curr_index]][next_index,2]),1)
+
+        # update r_map
+        r_map[[curr_index]][[next_index]]<-rbind(r_map[[curr_index]][[next_index]], reward_entry)
+
+        # update q_map
+        q_entry<-as.numeric(q_map[[curr_index]][next_index, 2])
+        r_entry<-mean(r_map[[curr_index]][[next_index]][,1] , na.rm=TRUE)
+        q_prime_ind<-which(indexer==indexer_next)
+        q_prime_val<-min(as.numeric(q_map[[q_prime_ind]][,2]))
+        q_map[[curr_index]][next_index, 2]<-q_score(q_entry, r_entry, q_prime_val)
+    }
+    return(list(q_map, r_map))
+}
+
+# epsilon-decision making policy
+# with probability epsilon, select next state based on best q
+# with prob 1-epsilon, select randomly
+q_choice<-function(map, e) {
+    rand_val<-runif(1)
+    if (rand_val <= e) { # choose best q
+        return(map[which.min(map[,2]),1])
+    } else { return(map[sample(1:nrow(map),1),1]) }
+}
+
+q_score<-function(q, r, q_prime) {
+    new_qsc<-q + alpha*(r + gamma*q_prime - q)
+    return(new_qsc)
+}
+
+ql_path_soln<-function(q_map) {
+    path_soln<-list(indexer_init)
+
+    # until final state in path is target state, keep searching unless have break
+    while (path_soln[[length(path_soln)]] != indexer_target) {
+        prev_ind<-which(indexer==path_soln[[length(path_soln)]])
+        prev_q<-q_map[[prev_ind]]
+        next_ind<-which.min(prev_q[,2])
+        next_path<-prev_q[next_ind,1]
+
+        if (next_path %in% path_soln) { # if path contains loop, break and return no path
+            path_soln<-NA
+            break
+        } else {
+            path_soln[[length(path_soln)+1]]<-next_path
+        }
+    }
+    return(path_soln)
+}
+
+# potentially replace with a score-convergence based on the Q map?
+ql_path_cost<-function(r_map, path_soln) {
+    cost<-0
+    if (any(is.na(path_soln))) { cost<-NA 
+    } else {
+        for (i in 1:(length(path_soln)-1)) {
+            prev_ind<-which(indexer==path_soln[[i]])
+            cost_ind<-which(names(r_map[[prev_ind]])==path_soln[i+1])
+            step_cost<-mean(r_map[[prev_ind]][[cost_ind]][,1] , na.rm=TRUE)
+            cost<-cost+step_cost
+        }
+    }   
+    return(cost)
+}
+
+epsilon_update<-function(epsilon_curr, path_conv, cost_delta) {
+    # note all epsilons cap at 1
+
+    if (path_conv) { # if path is converged
+        if (cost_delta>0.25) { # but value is really noisy, don't change randomness
+            epsilon_new<-epsilon_curr
+        } else { epsilon_new<-min(1, epsilon_curr+0.025) } # if value is reasonably stable, reduce randomness
+    } else { #if path isn't converged
+        if (cost_delta<0.1) { # but the values between competing paths are reasonably close, reduce randomness
+            epsilon_new<-min(1, epsilon_curr+0.025)
+        } else { epsilon_new<-epsilon_curr } 
+    }
+    return(epsilon_new)
+}
+
+#### for testing only
+ql_path_cost2<-function(r_map, path_soln) {
+    cost<-list()
+    if (any(is.na(path_soln))) { cost<-NA 
+    } else {
+        for (i in 1:(length(path_soln)-1)) {
+            prev_ind<-which(indexer==path_soln[[i]])
+            cost_ind<-which(names(r_map[[prev_ind]])==path_soln[i+1])
+            step_cost<-mean(r_map[[prev_ind]][[cost_ind]][,1] , na.rm=TRUE)
+            cost[[i]]<-step_cost
+        }
+    }   
+    return(cost)
+}
+
+split_func<-function(x) { 
+    if(any(is.na(x))) { out<-NA
+    } else { out<-as.numeric(unlist(strsplit(x, split='_'))) }
+    return(out)
+}
---
+++ Q-learning/norm-norm-2d/ql_grid02_setup.r
@@ -0,0 +1,381 @@
+# ql_grid_setup.r
+# maze grid version - doesn't need all the SIS and distribution portions
+
+# set up grid for Q-learning search in lambda-T parameter space
+# to be called by ql_master.r - has depends on params defined in that file
+
+##################
+# set up grid
+##################
+
+lambda_min<-0
+lambda_max<-1
+
+lambda_points<-seq(from=lambda_min, to=lambda_max, length.out=lambda_numpoints)
+temp_points<-seq(from=temp_min, to=temp_max, length.out=temp_numpoints) # note: temp means temperature, not temporary
+
+# list of length 2 containing param values
+point_lists<-list(lambda_points, temp_points)
+names(point_lists)<-c('lambda', 'temperature')
+
+# matrix containing combinations of param values
+point_grid<-expand.grid(point_lists)
+
+# list of combinations of param values
+point_list<-as.list(as.data.frame(t(point_grid)))
+
+# unique string identifiers for each param combination
+indexer<-apply(point_grid, 1, paste, collapse='_')
+
+# set target temperature and key points
+temp_target<-which.min(abs(temp_points-1.5))
+state_init<-c(lambda_points[1], temp_points[temp_target])
+state_target<-c(lambda_points[lambda_numpoints], temp_points[temp_target])
+indexer_init<-paste(state_init, collapse='_')
+indexer_target<-paste(state_target, collapse='_')
+
+##############
+# movement options
+##############
+
+# define degrees of freedom and maximum displacement for each
+
+dof<-c('TRUE', temp_dof)
+names(dof)<-c('lambda', 'temp')
+
+dof_ranges<-list()
+dof_ranges[1]<-list(0:move_jump)
+if (dof[2]) { dof_ranges[2]<-list(-move_jump : move_jump)
+    }  else { dof_ranges[2]<-list(NA) }
+names(dof_ranges)<-names(dof)
+
+nacheck<-function(x) { return(any(is.na(x))) }
+
+get_all_moves<-function() {
+    grid_full<-expand.grid(dof_ranges)
+    grid_strip<-grid_full[ , !apply(grid_full, 2, nacheck), drop=F] # remove NA columns that mess with absolute value
+    grid_trim<-grid_strip[rowSums(abs(grid_strip))<=move_jump, , drop=F] # filter out actions with too many moves
+    grid_trim2<-grid_trim[rowSums(abs(grid_trim))>0, , drop=F] # filter out null moves
+    ind<-as.numeric(rownames(grid_trim2)) # pull out good rows
+    grid_out<-grid_full[ind, ]
+    return(grid_out)
+}
+
+move_universe<-get_all_moves()
+
+equivcheck<-function(list, point) { isTRUE(all.equal(list, point)) }
+
+# movement primitive
+move_result<-function(move, state) {
+
+    # compute new state based on original state and set of changes from move
+    new_pos<-rep(NA, length.out=length(state))
+
+    for (i in 1:length(move)) { # for each dimension
+        if (!is.na(move[i])) { # if that dimension is varying
+            dimension<-point_lists[[i]] # get possible values
+            curr_ind<-which(sapply(dimension, equivcheck, state[i]))
+            new_ind<-curr_ind + as.numeric(move[i])
+            if (new_ind<=0) { new_pos[i]<-NA # if out of bounds, call it NA
+            } else if (new_ind>length(dimension)) { new_pos[i]<-NA 
+            } else { new_pos[i]<-dimension[new_ind] }
+        } else { new_pos[i]<-state[i] } # if move is NA, then retain position
+    } # note: output may have NAs in position - this is fine. NA moves are pruned by valid move checker
+    return(new_pos)
+}
+
+is_valid_move<-function(move, state) { 
+    pos<-paste(move_result(move, state), collapse='_')
+    if (pos %in% indexer) { return(TRUE) 
+    } else { return(FALSE) }
+}
+
+# create adjacency lists
+adj_list<-vector('list', length(indexer))
+names(adj_list)<-indexer
+for (i in 1:length(point_list)) {
+    move_bool<-apply(move_universe, 1, is_valid_move, point_list[[i]])
+    valid_moves<-move_universe[move_bool,]
+    neighbors<-t(apply(valid_moves, 1, move_result, point_list[[i]]))
+
+    # if first entry of point list is lambdamax, then only allow neighbors that are above/below such that it goes towards the target
+    if (point_list[[i]][1]==lambda_max) {
+        rm_ind<-vector()
+        if (point_list[[i]][2]>state_target[2]) {
+            rm_ind<-which(neighbors[,2]>point_list[[i]][2])
+        } else if (point_list[[i]][2]<state_target[2]) {
+            rm_ind<-which(neighbors[,2]<point_list[[i]][2])
+        }
+        if (length(rm_ind)!=0) { neighbors<-neighbors[-rm_ind, , drop=FALSE] }
+    }
+    adj_list[[i]]<-apply(neighbors, 1, paste, collapse='_')
+}
+
+
+##############
+# map setups
+##############
+
+q_map<-vector('list', length(indexer))
+names(q_map)<-indexer
+for (i in 1:length(point_list)) {
+
+    # for each point in the graph, create an entry to the Q map
+    # each entry is a matrix denoting the legal actions/next states and the Q value associated with that action
+    q_mat<-array(NA, dim=c(length(adj_list[[i]]), 2))
+    q_mat[,1]<-adj_list[[i]]
+    q_mat[,2]<-100 # arbitrary large value to bias path towards explored edges towards end of algorithm (decision reversed, see below)
+    q_map[[i]]<-q_mat
+}
+
+q_map[[which(names(q_map)==indexer_target)]][,2]<-0
+
+# consider initializing q to 0. 
+# need special handling for maps at target states - no moves are allowed, and the q score for adjacent states should just be the discounted reward
+
+r_map<-vector('list', length(indexer))
+names(r_map)<-indexer
+for (i in 1:length(point_list)) {
+
+    # for each point in the graph create an entry to the Rewards map
+    # each entry is a list indexed by the next states, where each list entry is a matrix showing guesses for the reward and their variances (if available)
+    r_list<-vector('list', length(adj_list[[i]]))
+    names(r_list)<-adj_list[[i]]
+    for (j in 1:length(adj_list[[i]])) {
+        r_list[[j]]<-array(NA, dim=c(1,2)) # initialize matrix with NAs for correct dimension. use na.rm option to remove them from future calculations
+    }
+    r_map[[i]]<-r_list
+}
+
+###################
+# user defined reward map
+###################
+
+# manually create a reward map lookup, so we can construct an example with known paths.
+# each reward is normally distributed with varying centers and scales
+# each traversal of an edge will draw a random reward from the corresponding distribution.
+
+r_distns<-vector('list', length(indexer))
+names(r_distns)<-indexer
+
+# for each state, create an entry to the list, detailing cost and variance (noise) for each exiting edge
+# 0, 0.5
+k<-1
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(10,1)
+r_d_temp[2,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.2, 0.5
+k<-2
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(10,1)
+r_d_temp[2,]<-c(2.5,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.4, 0.5
+k<-3
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(50,1)
+r_d_temp[2,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.6, 0.5
+k<-4
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(50,1)
+r_d_temp[2,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.8, 0.5
+k<-5
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(10,1)
+r_d_temp[2,]<-c(2.5,1)
+r_distns[[k]]<-r_d_temp
+
+# 1, 0.5
+k<-6
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0, 1.375
+k<-7
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_d_temp[2,]<-c(10,1)
+r_d_temp[3,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.2, 1.375
+k<-8
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(2.5,1)
+r_d_temp[2,]<-c(10,1)
+r_d_temp[3,]<-c(2.5,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.4, 1.375
+k<-9
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_d_temp[2,]<-c(1,1)
+r_d_temp[3,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.6, 1.375
+k<-10
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_d_temp[2,]<-c(50,1)
+r_d_temp[3,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.8, 1.375
+k<-11
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(2.5,1)
+r_d_temp[2,]<-c(10,1)
+r_d_temp[3,]<-c(2.5,1)
+r_distns[[k]]<-r_d_temp
+
+# 1, 1.375
+k<-12
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_d_temp[2,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0, 2.25
+k<-13
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_d_temp[2,]<-c(10,1)
+r_d_temp[3,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.2, 2.25
+k<-14
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(2.5,1)
+r_d_temp[2,]<-c(1,1)
+r_d_temp[3,]<-c(2.5,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.4, 2.25
+k<-15
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_d_temp[2,]<-c(50,1)
+r_d_temp[3,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.6, 2.25
+k<-16
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_d_temp[2,]<-c(50,1)
+r_d_temp[3,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.8, 2.25
+k<-17
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(2.5,1)
+r_d_temp[2,]<-c(10,1)
+r_d_temp[3,]<-c(2.5,1)
+r_distns[[k]]<-r_d_temp
+
+# 1, 2.25
+k<-18
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0, 3.125
+k<-19
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_d_temp[2,]<-c(10,1)
+r_d_temp[3,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.2, 3.125
+k<-20
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(2.5,1)
+r_d_temp[2,]<-c(10,1)
+r_d_temp[3,]<-c(2.5,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.4, 3.125
+k<-21
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_d_temp[2,]<-c(50,1)
+r_d_temp[3,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.6, 3.125
+k<-22
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_d_temp[2,]<-c(1,1)
+r_d_temp[3,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.8, 3.125
+k<-23
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(2.5,1)
+r_d_temp[2,]<-c(10,1)
+r_d_temp[3,]<-c(2.5,1)
+r_distns[[k]]<-r_d_temp
+
+# 1, 3.125
+k<-24
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
+
+# 0, 4
+k<-25
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_d_temp[2,]<-c(10,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.2, 4
+k<-26
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(2.5,1)
+r_d_temp[2,]<-c(10,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.4, 4
+k<-27
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_d_temp[2,]<-c(50,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.6, 4
+k<-28
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_d_temp[2,]<-c(50,1)
+r_distns[[k]]<-r_d_temp
+
+# 0.8, 4
+k<-29
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(2.5,1)
+r_d_temp[2,]<-c(10,1)
+r_distns[[k]]<-r_d_temp
+
+# 1, 4
+k<-30
+r_d_temp<-array(NA, dim=dim(q_map[[k]]))
+r_d_temp[1,]<-c(1,1)
+r_distns[[k]]<-r_d_temp
